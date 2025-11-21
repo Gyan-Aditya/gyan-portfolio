9a40071e9e7f601c01680577ff55ac2c8473d037
Gyan Aditya <163598681+Gyan-Aditya@users.noreply.github.com>
2025-11-22 01:21:39 +0530

    Changes in project section and component for deployment

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,4 +1,7 @@
 import {motion} from 'motion/react';
+
+const BASE_URL = import.meta.env.BASE_URL;
+
 const ProjectDetails = ({title, description, subDescription, image, tags, href,closeModal}) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
@@ -6,7 +9,7 @@ const ProjectDetails = ({title, description, subDescription, image, tags, href,c
         <button onClick={closeModal} className="absolute p-2 rounded-sm top-5 right-5 bg-midnight hover:bg-gray-500">
             <img src='assets/close.svg' className="w-6 h-6"/>
         </button>
-        <img src={image} alt={title} className="w-full rounded-t-2xl"/>
+        <img src={`${BASE_URL}${image}`} alt={title} className="w-full rounded-t-2xl"/>
         <div className="p-5">
             <h5 className="mb-2 text-2xl font-bold text-white ">{title}</h5>
             <p className="mb-3 font-normal text-neutral-400">{description}</p>
@@ -14,7 +17,7 @@ const ProjectDetails = ({title, description, subDescription, image, tags, href,c
         </div>
         <div className="flex items-center justify-between mt-4">
             <div className="flex gap-3">
-                {tags.map((tag)=>(<img key={tag.id} src={tag.path} alt={tag.name} className="rounded-lg size-10 hover-animation"/>))}
+                {tags.map((tag)=>(<img key={tag.id} src={`${BASE_URL}${tag.path}`} alt={tag.name} className="rounded-lg size-10 hover-animation"/>))}
             </div>
             <a className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation"href={href}>
                 View Project <img src='assets/arrow-up.svg' className="size-4" />
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Project from "../components/Project"
 import { myProjects } from "../constants"
 import {motion, useMotionValue, useSpring} from 'motion/react';
+
+const BASE_URL = import.meta.env.BASE_URL;
+
 const Projects = () => {
   const x=useMotionValue(0);
   const y=useMotionValue(0);
@@ -17,7 +20,7 @@ const Projects = () => {
         <h2 className="text-heading">My Selected Projects</h2>
         <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full"/>
         {myProjects.map((project)=>(<Project key={project.id} {...project} setPreview={setPreview}/>))}
-        {preview && <motion.img className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80" src={preview} style={{x:springX, y:springY}}/>}
+        {preview && <motion.img className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80" src={`${BASE_URL}${preview}`} style={{x:springX, y:springY}}/>}
     </section>
   )
 }
